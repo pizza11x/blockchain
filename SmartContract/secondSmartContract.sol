b050f71231006d29bcf4f5b36d6ae88986cf3092
Antonio Sarno <83610904+pizza11x@users.noreply.github.com>
2021-08-15 00:44:10 +0200

    Update secondSmartContract.sol

--- SmartContract/secondSmartContract.sol
+++ SmartContract/secondSmartContract.sol
@@ -4,17 +4,14 @@ pragma solidity ^0.8.6;
 
 contract Token {
 
-    // My Variables
     string public name = "pizzoken";
     string public symbol = "Pzz";
     uint256 public decimals = 18;
     uint256 public totalSupply = 10000000000000000000000;
 
-    // Keep track balances and allowances approved
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    // Events - fire events on state changes etc
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
@@ -26,22 +23,12 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    /// @notice transfer amount of tokens to an address
-    /// @param _to receiver of token
-    /// @param _value amount value of token to send
-    /// @return success as true, for transfer 
     function transfer(address _to, uint256 _value) external returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
         _transfer(msg.sender, _to, _value);
         return true;
     }
 
-    /// @dev internal helper transfer function with required safety checks
-    /// @param _from, where funds coming the sender
-    /// @param _to receiver of token
-    /// @param _value amount value of token to send
-    // Internal function transfer can only be called by this contract
-    //  Emit Transfer Event event 
     function _transfer(address _from, address _to, uint256 _value) internal {
         // Ensure sending is to valid address! 0x0 address cane be used to burn() 
         require(_to != address(0));
@@ -50,12 +37,6 @@ contract Token {
         emit Transfer(_from, _to, _value);
     }
 
-    /// @notice Approve other to spend on your behalf eg an exchange 
-    /// @param _spender allowed to spend and a max amount allowed to spend
-    /// @param _value amount value of token to send
-    /// @return true, success once address approved
-    //  Emit the Approval event  
-    // Allow _spender to spend up to _value on your behalf
     function approve(address _spender, uint256 _value) external returns (bool) {
         require(_spender != address(0));
         allowance[msg.sender][_spender] = _value;
@@ -63,13 +44,6 @@ contract Token {
         return true;
     }
 
-    /// @notice transfer by approved person from original address of an amount within approved limit 
-    /// @param _from, address sending to and the amount to send
-    /// @param _to receiver of token
-    /// @param _value amount value of token to send
-    /// @dev internal helper transfer function with required safety checks
-    /// @return true, success once transfered from original account    
-    // Allow _spender to spend up to _value on your behalf
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
